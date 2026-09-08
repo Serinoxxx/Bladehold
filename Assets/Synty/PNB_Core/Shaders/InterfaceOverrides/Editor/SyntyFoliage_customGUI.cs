13c8369c65948dc9e992bbc18f1a441f8a2da529
serinoxxx <lancemclachlan@gmail.com>
2026-09-08 16:32:06 +1200

    added alpine biome

--- Assets/Synty/PNB_Core/Shaders/InterfaceOverrides/Editor/SyntyFoliage_customGUI.cs
+++ Assets/Synty/PNB_Core/Shaders/InterfaceOverrides/Editor/SyntyFoliage_customGUI.cs
@@ -91,7 +91,7 @@ public class SyntyFoliage_customGUI : ShaderGUI
             int renderFace = (int)material.GetFloat("_BUILTIN_CullMode");
             renderFace = EditorGUILayout.Popup("Render Face", renderFace, new string[] { "Both", "Back", "Front" });
             material.SetFloat("_BUILTIN_CullMode", renderFace);
-
+            
             // Alpha Clipping
             bool alphaClip = material.GetFloat("_BUILTIN_AlphaClip") == 1;
             alphaClip = EditorGUILayout.Toggle("Alpha Clipping", alphaClip);
@@ -108,6 +108,10 @@ public class SyntyFoliage_customGUI : ShaderGUI
                 material.DisableKeyword("_BUILTIN_AlphaClip");
             }
 
+            // Back Face Lighting
+            bool backFaceLighting = material.GetFloat("_Enable_Back_Face_Lighting") == 1;
+            backFaceLighting = EditorGUILayout.Toggle("Enable Back Face Lighting", backFaceLighting);
+            material.SetFloat("_Enable_Back_Face_Lighting", backFaceLighting ? 1 : 0);
 
             EditorGUILayout.EndVertical();
             EditorGUILayout.Separator();
@@ -174,6 +178,10 @@ public class SyntyFoliage_customGUI : ShaderGUI
                 material.DisableKeyword("_ALPHABLEND_ON");
             }
 
+            // Back Face Lighting
+            bool backFaceLightingURP = material.GetFloat("_Enable_Back_Face_Lighting") == 1;
+            backFaceLightingURP = EditorGUILayout.Toggle("Enable Back Face Lighting", backFaceLightingURP);
+            material.SetFloat("_Enable_Back_Face_Lighting", backFaceLightingURP ? 1 : 0);
 
             EditorGUILayout.EndVertical();
 
